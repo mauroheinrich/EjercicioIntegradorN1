d0527f22982ae033cf649f0148517322b66c4127
mauro heinrich <47451542+mauroheinrich@users.noreply.github.com>
2024-05-18 20:08:17 -0300

    orimer commit

---
+++ src/main/java/ar/com/mauroheinrich/TpIntegrador1/TpIntegrador1Application.java
@@ -0,0 +1,13 @@
+package ar.com.mauroheinrich.TpIntegrador1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegrador1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegrador1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/mauroheinrich/TpIntegrador1/TpIntegrador1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.mauroheinrich.TpIntegrador1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegrador1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
