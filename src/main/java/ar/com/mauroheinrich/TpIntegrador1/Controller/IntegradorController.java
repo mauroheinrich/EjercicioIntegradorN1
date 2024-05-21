5f73d60c6aa7e78dded8b20a4e23a4e9f55c3fe8
Mauro Jesus Heinrich <heinrichmauro@gmail.com>
2024-05-21 11:21:30 -0300

    finalizado

--- src/main/java/ar/com/mauroheinrich/TpIntegrador1/Controller/IntegradorController.java
+++ src/main/java/ar/com/mauroheinrich/TpIntegrador1/Controller/IntegradorController.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ar.com.mauroheinrich.TpIntegrador1.Controller;
 
-/**
- *
- * @author mauro
- */
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class IntegradorController {
+    @GetMapping("/conversion/{galones}")
+    public String galonesALitros(@PathVariable double galones){
+        
+       double litros = galones*3.78541;
+       
+       return "La cantidad de litros equivalentes a : " + galones + " galones" 
+               +" es de "+ litros + " litros" ;
+               
+    }
     
 }
